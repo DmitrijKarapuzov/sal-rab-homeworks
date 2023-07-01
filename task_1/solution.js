da5cd69820e992671f85a3c97562bf71d61bb69f
Dmitriy Karapuzov <dmitrijkarapuzov@gmail.com>
2023-07-01 17:39:34 +0300

    dz 1.3

--- task_1/solution.js
+++ task_1/solution.js
@@ -50,9 +50,13 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum;
+    newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText;
+    newSumText = newSum + "₽";
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    console.log(newSumText);
     // Конец решения задания №1.3.
 
     return newSumText;
